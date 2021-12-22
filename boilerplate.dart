3d7fe76ddd972aff907e9a9325b9f14427aa78d4
Sazmx <36517628+Sazmx@users.noreply.github.com>
2021-12-22 20:04:18 +0300

    Update boilerplate.dart

--- boilerplate.dart
+++ boilerplate.dart
@@ -4,7 +4,8 @@ void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
+  
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
